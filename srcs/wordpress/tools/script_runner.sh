c3af8107b3f9493206e73660e20dbb58f3f7b769
dependentmadani <madani.badaoui12@gmail.com>
2023-03-08 13:50:55 -0500

    working on wordpress

--- srcs/wordpress/tools/script_runner.sh
+++ srcs/wordpress/tools/script_runner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source .env
 # create directory to use in nginx container later and also to setup the wordpress conf
 mkdir /var/www/
 mkdir /var/www/html
@@ -12,58 +13,67 @@ rm -rf *
 # The commands are for installing and using the WP-CLI tool.
 
 # downloads the WP-CLI PHAR (PHP Archive) file from the GitHub repository. The -O flag tells curl to save the file with the same name as it has on the server.
-# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 
 # # makes the WP-CLI PHAR file executable.
-# chmod +x wp-cli.phar 
+chmod +x wp-cli.phar 
 
 # # moves the WP-CLI PHAR file to the /usr/local/bin directory, which is in the system's PATH, and renames it to wp. This allows you to run the wp command from any directory
-# mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
 
-curl -LO https://wordpress.org/latest.tar.gz
+# curl -LO https://wordpress.org/latest.tar.gz
 
-tar -zxvf latest.tar.gz
+# tar -zxvf latest.tar.gz
 
-rm -rf /var/www/html/latest.tar.gz
+# rm -rf /var/www/html/latest.tar.gz
 
-cp wordpress/wp-config-sample.php wordpress/wp-config.php
+# cp wordpress/wp-config-sample.php wordpress/wp-config.php
 
-# cp -a /tmp/wordpress/. /var/www/
+# # cp -a /tmp/wordpress/. /var/www/
 
 chown -R www-data:www-data /var/www/html
 
 # downloads the latest version of WordPress to the current directory. The --allow-root flag allows the command to be run as the root user, which is necessary if you are logged in as the root user or if you are using WP-CLI with a system-level installation of WordPress.
-# wp core download --allow-root
+wp core download --allow-root
 
-# mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 # change the those lines in wp-config.php file to connect with database
 
 #line 23
-sed -i -r "s/database/$MYSQL_DATABASE/1"   wordpress/wp-config.php
+sed -i -r "s/database_name_here/$MYSQL_DATABASE/1"   wp-config.php
 #line 26
-sed -i -r "s/database_user/$MYSQL_USER:/1"  wordpress/wp-config.php
+sed -i -r "s/username_here/$MYSQL_USER/1"  wp-config.php
 #line 29
-sed -i -r "s/passwod/$MYSQL_PASSWORD/1"    wordpress/wp-config.php
+sed -i -r "s/password_here/$MYSQL_PASSWORD/1"    wp-config.php
 
 # #line 32
-sed -i -r "s/localhost/mariadb/1"    wordpress/wp-config.php
+sed -i -r "s/localhost/mariadb/1"    wp-config.php
+
+# sed -i -r "s/'AUTH_KEY',    'put your unique phrase here'/ 2<A|[TIWhqOH1ZH@W5FTQYSq2eQwU8IJjt8KUA/q[ndEb+5USstzcYbkgK7}ENq/1" wp-config.php
+# sed -i -r "0,/'put your unique phrase here'/s/'put your unique phrase here'/)Y9V-T.E4mOiajDnQqP-rs+K:/Lg:oz*lD``)cbc]dd[46L{@_]8%H_~u!QN78v]/" wp-config.php
+# sed -i -r "0,/'put your unique phrase here'/s/'put your unique phrase here'/$ HqfIHfKxza;-3P!OZyZj+[Y+U5he%,d+KN)S9AMLs%pY//k^Y~RQn@oxYLaj/" wp-config.php
+# sed -i -r "0,/'put your unique phrase here'/s/'put your unique phrase here'/|xXx9SE2d-ZQS=9^OxA2l@k5pL|wzBpk)]+N+^tVQ}4Fp4si-?~C|FE1s$^sA6/" wp-config.php
+# sed -i -r "0,/'put your unique phrase here'/s/'put your unique phrase here'/2{b2U&E7uuyG^KxKMUKjXFDsf%%><)Wl1GNzUs{Wd2aG]Zg7bng^)%qxMf}wg9/" wp-config.php
+# sed -i -r "0,/'put your unique phrase here'/s/'put your unique phrase here'/bWUXLL5QBJFYgEPtQf|-h0k1:.>->r7h|EF^+lzwTBE^[1dQ=[<>,+U0~]M]t/" wp-config.php
+# sed -i -r "0,/'put your unique phrase here'/s/'put your unique phrase here'/;M%cQV(O3 gW)yP>^pj+=}27jB?zkqxPohJ8Qz}b|-xh`osL.F#6h3c`cOEZaF/" wp-config.php
+# sed -i -r "0,/'put your unique phrase here'/s/'put your unique phrase here'/-E)J{-}fa9aRv-h+A2.!b06+zOqi:XhYVqrRyRgr|}tR@apFgz]iq9,F{2FCW/" wp-config.php
 
 # installs WordPress and sets up the basic configuration for the site. The --url option specifies the URL of the site, --title sets the site's title, --admin_user and --admin_password set the username and password for the site's administrator account, and --admin_email sets the email address for the administrator. The --skip-email flag prevents WP-CLI from sending an email to the administrator with the login details.
-# wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=$DOMAIN_NAME/ --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USR --admin_password=$WORDPRESS_ADMIN_PWD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
 
 # # creates a new user account with the specified username, email address, and password. The --role option sets the user's role to author, which gives the user the ability to publish and manage their own posts.
-# wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp user create $WORDPRESS_USR $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PWD --allow-root
 
 # # installs the Astra theme and activates it for the site. The --activate flag tells WP-CLI to make the theme the active theme for the site.
-# wp theme install astra --activate --allow-root
+wp theme install astra --activate --allow-root
 
 
 # # uses the sed command to modify the www.conf file in the /etc/php/7.3/fpm/pool.d directory. The s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g command substitutes the value 9000 for /run/php/php7.3-fpm.sock throughout the file. This changes the socket that PHP-FPM listens on from a Unix domain socket to a TCP port.
-# sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 # # creates the /run/php directory, which is used by PHP-FPM to store Unix domain sockets.
-# mkdir /run/php
+mkdir /run/php
 
 
 # wp redis enable --allow-root
