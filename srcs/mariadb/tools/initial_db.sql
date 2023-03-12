3595d8bb3ef69c261f497e51558961806ff3ca97
dependentmadani <madani.badaoui12@gmail.com>
2023-03-12 11:44:58 -0400

    still the error establishing a database connection

--- srcs/mariadb/tools/initial_db.sql
+++ srcs/mariadb/tools/initial_db.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
--- FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'mbadaoui'@'%' IDENTIFIED BY 'hamid';
--- FLUSH PRIVILEGES;
 GRANT ALL privileges ON wordpress.* TO 'mbadaoui'@'%';
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'hamida';
